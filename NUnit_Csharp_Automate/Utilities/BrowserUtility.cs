43152376caa1a133ef3ad3de82f639544fc91d89
abdul-qadir92 <92802404+abdul-qadir92@users.noreply.github.com>
2022-03-15 01:09:02 +0530

    Add files via upload

---
+++ NUnit_Csharp_Automate/Utilities/BrowserUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace NUnit_Csharp_Automate.Utilities
+{
+    public class BrowserUtility
+    {
+        public IWebDriver Init(IWebDriver driver)
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            return driver;
+        }
+
+    }
+}
